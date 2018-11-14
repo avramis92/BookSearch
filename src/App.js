7d521ad9da8247e4855748812a3dbf905f86d6ad
spyros.avramis <spyros.avramis@accenture.com>
2018-11-14 11:44:16 +0100

    https://github.com/avramis92

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+  ReactiveBase,
+  DataSearch,
+  SingleRange,
+  ResultCard
+} from '@appbaseio/reactivesearch';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ReactiveBase
+        app="good-books-ds"
+        credentials="nY6NNTZZ6:27b76b9f-18ea-456c-bc5e-3a5263ebc63d"
+      >
+        <div className="navbar">
+          <div className="logo">
+            The Booksearch App
+          </div>
+          <DataSearch
+            componentId="mainSearch"
+            dataField={["original_title", "original_title.search", "authors", "authors.search"]}
+            queryFormat="and"
+            placeholder="Search for a book title or an author"
+            autosuggest={false}
+            className="datasearch"
+            innerClass={{
+              "input": "searchbox",
+              "list": "suggestionlist"
+            }}
+          />
+        </div>
+        <div className={"display"}>
+          <div className={"leftSidebar"}>
+            <SingleRange
+              componentId="ratingsFilter"
+              dataField="average_rating_rounded"
+              title="Book Ratings"
+              data={[
+                { start: 4, end: 5, label: "★★★★ & up" },
+                { start: 3, end: 5, label: "★★★ & up" },
+                { start: 2, end: 5, label: "★★ & up" },
+                { start: 1, end: 5, label: "★ & up" },
+              ]}
+            />
+          </div>
+          <div className={"mainBar"}>
+            <ResultCard
+              componentId="results"
+              dataField="original_title"
+              react={{
+                "and": ["mainSearch", "ratingsFilter"]
+              }}
+              pagination={true}
+              size={8}
+              onData={(res)=>(
+                {
+                  "image": res.image,
+                  "title": res.original_title,
+                  "description":  res.average_rating + " ★ "
+                }
+              )}
+              className="result-data"
+              innerClass={{
+                "image": "result-image",
+                "resultStats": "result-stats"
+              }}
+            />
+          </div>
+        </div>
+      </ReactiveBase>
     );
   }
 }
